76ef99cef2aef246eb9b400a0106ae763868de0c
Efdal Gunbay <efdal.gunbay@gmail.com>
2023-09-29 08:43:18 +0300

    fix

--- src/test/java/com/projectassist/runners/CukesRunner.java
+++ src/test/java/com/projectassist/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         glue = "com/projectassist/step_definitions",
         features = "src/test/resources/features",
         publish = true,
-        tags = "@Home",
+        tags = "@Reg",
         dryRun =false
 
 
